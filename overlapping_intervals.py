9c333dc8c7874eac54c24ba32da40c040bf2c35a
Alya Atlab <133522435+alya-atlab@users.noreply.github.com>
2024-11-05 13:17:01 +0200

    Fix The Function

--- overlapping_intervals.py
+++ overlapping_intervals.py
@@ -1,17 +1,29 @@
 def merge_intervals(intervals):
+    intervals.sort()
     new_list =[]
-    for i in intervals:
-        min_num = i[0]
-        max_num = i[1]
-        for j in intervals:
-            if max_num > j[0] and max_num < j[1]:
-                max_num = j[1]
-                intervals.remove(j)
-        
-        new_list.append([min_num , max_num]) 
+    min_num = intervals[0][0]
+    max_num = intervals[0][1]
+    print("min", min_num)
+    print("max", max_num)
+    print("-----------")
+    for i in intervals:              
+        if max_num >= i[0] :
+            if(max_num < i[1]):
+                max_num = i[1]
+            print("min", min_num)
+            print("max", max_num) 
+                       
+        else:
+            new_list.append([min_num , max_num]) 
+            print("min", min_num)
+            print("max", max_num)
+            min_num = i[0]
+            max_num = i[1]
+                 
+    new_list.append([min_num , max_num]) 
         
     return new_list
     
-print(merge_intervals( [ [1,2], [2,4], [6,8]]))      
+print(merge_intervals([[1,3], [2,4], [6,8], [3,6]]))      
             
         
